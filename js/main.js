fa130a1661b0dc755de08b2e14c3e2416d08c4a1
eliewebavenue <elie@webavenue.agency>
2024-10-08 11:36:44 +0200

    Au click sur jouer contre l'ordi cela affiche #game-settings et au click sur #close-game-settings ca cache #game-settings

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 let gameRules = document.getElementById('game-rules');
 let gameHome = document.getElementById('game-home');
+let gameSettings = document.getElementById('game-settings');
+
 let buttonOpenGameRules = document.getElementById('open-game-rules');
 let buttonOpenGameHome = document.getElementById('open-game-home');
+let buttonOpenGameSettings = document.getElementById('open-game-settings');
+let buttonCloseGameSettings = document.getElementById('close-game-settings');
 
 function openGameRules(){
     gameRules.style.display = 'block';
@@ -11,8 +15,17 @@ function openGameHome(){
     gameRules.style.display = 'none';
     gameHome.style.display = 'block';
 }
+function openGameSettings(){
+    gameSettings.style.display = 'block';
+}
+function closeGameSettings(){
+    gameSettings.style.display = 'none';
+}
 
 buttonOpenGameRules.addEventListener('click', openGameRules);
 
 buttonOpenGameHome.addEventListener('click', openGameHome);
 
+buttonOpenGameSettings.addEventListener('click', openGameSettings);
+
+buttonCloseGameSettings.addEventListener('click', closeGameSettings);
