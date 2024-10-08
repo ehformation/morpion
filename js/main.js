230d312556d21ba1cf84b9fae3b297456531af0b
eliewebavenue <elie@webavenue.agency>
2024-10-08 11:25:10 +0200

    Au click sur #open-game-home cela affiche la section #game-home, et ca cache #game-rules

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 let gameRules = document.getElementById('game-rules');
 let gameHome = document.getElementById('game-home');
 let buttonOpenGameRules = document.getElementById('open-game-rules');
+let buttonOpenGameHome = document.getElementById('open-game-home');
 
 function openGameRules(){
     gameRules.style.display = 'block';
     gameHome.style.display = 'none';
 }
+function openGameHome(){
+    gameRules.style.display = 'none';
+    gameHome.style.display = 'block';
+}
 
 buttonOpenGameRules.addEventListener('click', openGameRules);
+
+buttonOpenGameHome.addEventListener('click', openGameHome);
 
